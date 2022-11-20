24aebc98218c37b817b69cb151142fbe9ab0c422
Vaughn Kottler <vaughnkottler@gmail.com>
2022-11-20 01:29:38 -0600

    More cleaning up and preparing workflow

--- console/index.ts
+++ console/index.ts
@@ -1,8 +1,18 @@
-export const app_elem = document.body.children.namedItem("app");
+/*
+ * Application entry-point.
+ */
+export function init(root_elem: Element) {
+  setInterval(() => {
+    root_elem.innerHTML = new Date().getTime().toString();
+  }, 100);
 
-const overlay_elem = document.body.children.namedItem("overlay");
-console.log(overlay_elem);
+  console.log("Application initialized.");
+}
 
-setInterval(() => {
-  app_elem.innerHTML = new Date().getTime().toString();
-}, 100);
+/*
+ * Initialize the application.
+ */
+const app_root = document.body.children.namedItem("app");
+if (app_root) {
+  init(app_root);
+}
