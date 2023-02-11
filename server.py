65c96bb6fb4363b6ef961992c1c82dbf3e949b20
Vaughn Kottler <vaughnkottler@gmail.com>
2023-02-11 17:48:16 -0600

    Another iteration on server script

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ A sample WebSocket server script.
 
 # built-in
 import asyncio
+import logging
 import sys
 from typing import List
 
@@ -24,20 +25,19 @@ async def server_init(conn: WebsocketConnection) -> bool:
 async def _main(stop_sig: asyncio.Event, args: List[str]) -> int:
     """Event-loop application entry-point."""
 
-    async with WebsocketConnection.serve(
-        server_init, host=args[0], port=int(args[1])
-    ) as server:
-        for sock in server.sockets:
-            host = sock.getsockname()
-            print(f"Serving on: '{host[0]}:{host[1]}'.")
-
-        await stop_sig.wait()
-
+    await WebsocketConnection.app(
+        stop_sig, server_init, host=args[0], port=int(args[1])
+    )
     return 0
 
 
 def main(args: List[str]) -> int:
     """Application entry-point."""
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(name)-36s - %(levelname)-6s - %(message)s",
+    )
 
     stop_sig = asyncio.Event()
     return run_handle_stop(stop_sig, _main(stop_sig, args))
