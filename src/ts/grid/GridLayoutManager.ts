d88097cff5da7dc41a879f4a4a9ec078eb496cb8
Vaughn Kottler <vaughnkottler@gmail.com>
2023-03-07 01:55:49 -0600

    Improve test coverage, add expand and contract

--- src/ts/grid/GridLayoutManager.ts
+++ src/ts/grid/GridLayoutManager.ts
@@ -1,3 +1,5 @@
+import assert from "assert";
+
 import { eventDirection, translationName } from "../cartesian/Translation";
 import { AreaUpdateHandler, GridArea } from "./GridArea";
 import { GridDimensions } from "./GridDimensions";
@@ -12,13 +14,27 @@ type GridResizeHandler = (event: CustomEvent<GridDimensions>) => void;
 type GridErrorHandler = (event: CustomEvent<string>) => void;
 type GridAreaUpdateHandler = (event: CustomEvent<GridArea>) => void;
 
+export type ElementArea = {
+  element: HTMLElement;
+  area: GridArea;
+};
+
+export type CursorUpdate = {
+  prev: ElementArea | undefined;
+  curr: ElementArea | undefined;
+};
+
+const CURSOR_UPDATE = "cursorUpdate";
+
+type CursorUpdateHandler = (event: CustomEvent<CursorUpdate>) => void;
+
 export class GridLayoutManager {
   container: HTMLElement;
   layout: GridLayout;
   elements: Map<number, HTMLElement>;
   areaUpdateHandler: AreaUpdateHandler;
 
-  constructor(container: HTMLElement) {
+  constructor(container: HTMLElement, cursorHandler?: CursorUpdateHandler) {
     this.container = container;
     this.container.style.display = "grid";
 
@@ -26,12 +42,20 @@ export class GridLayoutManager {
     this.container.style.gridAutoRows = "1fr";
     this.container.style.gridAutoColumns = "1fr";
 
-    this.layout = new GridLayout();
+    this.layout = new GridLayout(this.#handleCursorUpdate.bind(this));
     this.elements = new Map<number, HTMLElement>();
     this.areaUpdateHandler = this.#handleAreaUpdate.bind(this);
 
     /* Apply the initial, empty layout. */
     this.layout.apply(this.container);
+
+    /* Register a cursor handler if one was provided. */
+    if (cursorHandler != undefined) {
+      this.container.addEventListener(
+        CURSOR_UPDATE,
+        cursorHandler as EventListener
+      );
+    }
   }
 
   #handleAreaUpdate(area: GridArea) {
@@ -41,6 +65,31 @@ export class GridLayoutManager {
         new CustomEvent<GridArea>(GRID_AREA_UPDATE, { detail: area })
       );
     }
+  }
+
+  getElementArea(areaId: number): ElementArea | undefined {
+    let result = undefined;
+
+    const area = this.layout.byId.get(areaId);
+    if (area != undefined) {
+      const element = this.elements.get(areaId);
+      assert(element != undefined);
+      result = { element: element, area: area };
+    }
+
+    return result;
+  }
+
+  #handleCursorUpdate(prevCursor: number, currCursor: number) {
+    this.getElementArea(currCursor);
+    this.container.dispatchEvent(
+      new CustomEvent<CursorUpdate>(CURSOR_UPDATE, {
+        detail: {
+          prev: this.getElementArea(prevCursor),
+          curr: this.getElementArea(currCursor),
+        },
+      })
+    );
   }
 
   createArea(
@@ -58,9 +107,10 @@ export class GridLayoutManager {
       this.areaUpdateHandler
     );
 
-    if (this.layout.validArea(area, true)) {
-      this.elements.set(area.areaId, element);
+    /* Set this early, in case we need it in the cursor-update handler. */
+    this.elements.set(area.areaId, element);
 
+    if (this.layout.validArea(area, true)) {
       /* Ensure the layout change gets applied. */
       this.container.appendChild(element);
       this.layout.apply(this.container);
@@ -74,6 +124,8 @@ export class GridLayoutManager {
       this.#handleAreaUpdate(area);
 
       return element;
+    } else {
+      this.elements.delete(area.areaId);
     }
   }
 
@@ -100,6 +152,44 @@ export class GridLayoutManager {
 
   registerErrorHandler(handler: GridErrorHandler) {
     this.container.addEventListener(ERROR_MESSAGE, handler as EventListener);
+  }
+
+  resizeCursorAreaHandler(event: KeyboardEvent, isExpand: boolean): boolean {
+    let resizeResult = false;
+
+    const direction = eventDirection(event);
+    if (direction != undefined) {
+      if (isExpand) {
+        resizeResult = this.layout.expandCursorArea(direction, this.container);
+        if (!resizeResult) {
+          this.#fireErrorMessage(
+            `Couldn't expand cursor area ${translationName(direction)}.`
+          );
+        }
+      } else {
+        resizeResult = this.layout.contractCursorArea(
+          direction,
+          this.container
+        );
+        this.#fireErrorMessage(
+          `Couldn't contract cursor area ${translationName(direction)}.`
+        );
+      }
+
+      /* Detect if resizing an area triggered normalization. */
+      if (resizeResult && this.layout.isNormal) {
+        this.#fireGridResize();
+      }
+    }
+    return direction != undefined;
+  }
+
+  expandCursorAreaHandler(event: KeyboardEvent): boolean {
+    return this.resizeCursorAreaHandler(event, true);
+  }
+
+  contractCursorAreaHandler(event: KeyboardEvent): boolean {
+    return this.resizeCursorAreaHandler(event, false);
   }
 
   resizeHandler(event: KeyboardEvent, isExpand: boolean): boolean {
